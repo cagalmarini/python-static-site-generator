36c1b03bfb8572586431dcb985b27d03e8b7d258
Carlos <cagalmarini@hotmail.com>
2021-12-19 11:16:08 -0300

    Delete ResourceParser.py

--- ssg/ResourceParser.py
+++
@@ -1,6 +0,0 @@
-
-class ResourceParser(Parser):
-    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
-
-    def parse(self, path : Path, source : Path, dest : Path):
-        self.copy(path, source, dest)
