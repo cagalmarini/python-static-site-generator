5d801a1f4b60db09165ee8192b74cf6cfcb2fd07
Carlos <cagalmarini@hotmail.com>
2021-12-19 10:05:45 -0300

    Update ssg.py

--- ssg.py
+++ ssg.py
@@ -1,13 +1,12 @@
 import typer
 from ssg.site import Site
 
-def main(self, source="content", dest="dist"):
+def main(source="content", dest="dist"):
     config = {
         "source": source,
         "dest": dest
     }
 
-    site = Site(**config)
-    site.build()
+    Site(**config).build()
 
 typer.run(main)
