b0464b24651318abd0973b8ff58127ebf88aa354
Carlos <cagalmarini@hotmail.com>
2021-12-26 11:01:51 -0300

    Update site.py
    -

--- ssg/site.py
+++ ssg/site.py
@@ -28,4 +28,4 @@ class Site:
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
-            raise NotImplementedError
+            print('Not implemented')
